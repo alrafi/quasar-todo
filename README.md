80b491bbddb631bb3da4f4f2778747e985593485
alrafi <hafisalrafi@gmail.com>
2021-02-11 14:21:27 +0700

    release app

---
+++ README.md
@@ -0,0 +1,21 @@
+# Quasar Todo (quasar-todo)
+
+A todo app build with Quasar
+
+## Install the dependencies
+
+```bash
+npm install
+```
+
+### Start the app in development mode (hot-code reloading, error reporting, etc.)
+
+```bash
+quasar dev
+```
+
+### Build the app for production
+
+```bash
+quasar build
+```
---
+++ src/pages/Help.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Help</h5>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum saepe,
+      sunt ullam eum, aspernatur quasi officia vitae ipsam voluptates enim
+      distinctio vero, aperiam sit similique laudantium at culpa doloribus
+      dolor.
+    </p>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis
+      voluptatibus possimus vitae dicta molestias minus quam labore ipsa
+      assumenda inventore, ipsam laboriosam atque sint expedita dolorum impedit
+      dolores nihil saepe?
+    </p>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: "Help"
+};
+</script>
+
+<style></style>
